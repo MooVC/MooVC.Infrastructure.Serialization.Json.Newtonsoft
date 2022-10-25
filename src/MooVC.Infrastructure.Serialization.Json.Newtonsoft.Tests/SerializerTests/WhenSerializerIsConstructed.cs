1c4c7f5f6503e7c5554cdbbfa30310591b638f89
MooVC <50200071+MooVC@users.noreply.github.com>
2022-10-25 15:07:18 +0100

    - Updated to target MooVC 7.x (**Breaking Change**).

--- src/MooVC.Infrastructure.Serialization.Json.Newtonsoft.Tests/SerializerTests/WhenSerializerIsConstructed.cs
+++ src/MooVC.Infrastructure.Serialization.Json.Newtonsoft.Tests/SerializerTests/WhenSerializerIsConstructed.cs
@@ -1,110 +1,85 @@
-﻿namespace MooVC.Infrastructure.Serialization.Json.Newtonsoft.SerializerTests
+﻿namespace MooVC.Infrastructure.Serialization.Json.Newtonsoft.SerializerTests;
+
+using System.Text;
+using global::Newtonsoft.Json;
+using Xunit;
+
+public sealed class WhenSerializerIsConstructed
 {
-    using System.Text;
-    using global::Newtonsoft.Json;
-    using Xunit;
-
-    public sealed class WhenSerializerIsConstructed
+    [Fact]
+    public void GivenNoSettingsThenADefaultSerializerIsCreated()
     {
-        [Fact]
-        public void GivenNoSettingsThenADefaultSerializerIsCreated()
+        var serializer = new Serializer();
+        var settings = new JsonSerializerSettings();
+
+        AssertEqual(Serializer.DefaultBufferSize, Serializer.DefaultEncoding, serializer, settings);
+    }
+
+    [Fact]
+    public void GivenABufferSizeThenASerializerIsCreatedWithTheBufferSizeApplied()
+    {
+        const int BufferSize = 32;
+        var serializer = new Serializer(bufferSize: 32);
+        var settings = new JsonSerializerSettings();
+
+        AssertEqual(BufferSize, Serializer.DefaultEncoding, serializer, settings);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(1)]
+    public void GivenABelowMinimumBufferSizeThenASerializerIsCreatedWithTheMinimumBufferSizeApplied(int bufferSize)
+    {
+        var serializer = new Serializer(bufferSize: bufferSize);
+        var settings = new JsonSerializerSettings();
+
+        AssertEqual(Serializer.MinimumBufferSize, Serializer.DefaultEncoding, serializer, settings);
+    }
+
+    [Fact]
+    public void GivenAEncodingThenASerializerIsCreatedWithTheEncodingApplied()
+    {
+        Encoding encoding = Encoding.ASCII;
+        var serializer = new Serializer(encoding: encoding);
+        var settings = new JsonSerializerSettings();
+
+        AssertEqual(Serializer.DefaultBufferSize, encoding, serializer, settings);
+    }
+
+    [Fact]
+    public void GivenSettingsThenASerializerIsCreatedWithTheSettingsApplied()
+    {
+        var settings = new JsonSerializerSettings
         {
-            var serializer = new Serializer();
-            var settings = new JsonSerializerSettings();
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
+            NullValueHandling = NullValueHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+            TypeNameHandling = TypeNameHandling.All,
+            TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+        };
 
-            AssertEqual(
-                Serializer.DefaultBufferSize,
-                Serializer.DefaultEncoding,
-                serializer,
-                settings);
-        }
+        var serializer = new Serializer(settings: settings);
 
-        [Fact]
-        public void GivenABufferSizeThenASerializerIsCreatedWithTheBufferSizeApplied()
-        {
-            const int BufferSize = 32;
-            var serializer = new Serializer(bufferSize: 32);
-            var settings = new JsonSerializerSettings();
+        AssertEqual(Serializer.DefaultBufferSize, Serializer.DefaultEncoding, serializer, settings);
+    }
 
-            AssertEqual(
-                BufferSize,
-                Serializer.DefaultEncoding,
-                serializer,
-                settings);
-        }
+    private static void AssertEqual(int bufferSize, Encoding encoding, Serializer serializer, JsonSerializerSettings settings)
+    {
+        Assert.Equal(bufferSize, serializer.BufferSize);
+        Assert.Equal(encoding, serializer.Encoding);
+        Assert.Equal(settings.DateTimeZoneHandling, serializer.Json.DateTimeZoneHandling);
 
-        [Theory]
-        [InlineData(0)]
-        [InlineData(-1)]
-        [InlineData(1)]
-        public void GivenABelowMinimumBufferSizeThenASerializerIsCreatedWithTheMinimumBufferSizeApplied(int bufferSize)
-        {
-            var serializer = new Serializer(bufferSize: bufferSize);
-            var settings = new JsonSerializerSettings();
+        AssertEqual(settings, serializer);
+    }
 
-            AssertEqual(
-                Serializer.MinimumBufferSize,
-                Serializer.DefaultEncoding,
-                serializer,
-                settings);
-        }
-
-        [Fact]
-        public void GivenAEncodingThenASerializerIsCreatedWithTheEncodingApplied()
-        {
-            Encoding encoding = Encoding.ASCII;
-            var serializer = new Serializer(encoding: encoding);
-            var settings = new JsonSerializerSettings();
-
-            AssertEqual(
-                Serializer.DefaultBufferSize,
-                encoding,
-                serializer,
-                settings);
-        }
-
-        [Fact]
-        public void GivenSettingsThenASerializerIsCreatedWithTheSettingsApplied()
-        {
-            var settings = new JsonSerializerSettings
-            {
-                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
-                DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
-                NullValueHandling = NullValueHandling.Ignore,
-                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
-                TypeNameHandling = TypeNameHandling.All,
-                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
-            };
-
-            var serializer = new Serializer(settings: settings);
-
-            AssertEqual(
-                Serializer.DefaultBufferSize,
-                Serializer.DefaultEncoding,
-                serializer,
-                settings);
-        }
-
-        private static void AssertEqual(
-            int bufferSize,
-            Encoding encoding,
-            Serializer serializer,
-            JsonSerializerSettings settings)
-        {
-            Assert.Equal(bufferSize, serializer.BufferSize);
-            Assert.Equal(encoding, serializer.Encoding);
-            Assert.Equal(settings.DateTimeZoneHandling, serializer.Json.DateTimeZoneHandling);
-
-            AssertEqual(settings, serializer);
-        }
-
-        private static void AssertEqual(JsonSerializerSettings expected, Serializer serializer)
-        {
-            Assert.Equal(expected.DefaultValueHandling, serializer.Json.DefaultValueHandling);
-            Assert.Equal(expected.NullValueHandling, serializer.Json.NullValueHandling);
-            Assert.Equal(expected.ReferenceLoopHandling, serializer.Json.ReferenceLoopHandling);
-            Assert.Equal(expected.TypeNameHandling, serializer.Json.TypeNameHandling);
-            Assert.Equal(expected.TypeNameAssemblyFormatHandling, serializer.Json.TypeNameAssemblyFormatHandling);
-        }
+    private static void AssertEqual(JsonSerializerSettings expected, Serializer serializer)
+    {
+        Assert.Equal(expected.DefaultValueHandling, serializer.Json.DefaultValueHandling);
+        Assert.Equal(expected.NullValueHandling, serializer.Json.NullValueHandling);
+        Assert.Equal(expected.ReferenceLoopHandling, serializer.Json.ReferenceLoopHandling);
+        Assert.Equal(expected.TypeNameHandling, serializer.Json.TypeNameHandling);
+        Assert.Equal(expected.TypeNameAssemblyFormatHandling, serializer.Json.TypeNameAssemblyFormatHandling);
     }
 }
--- src/MooVC.Infrastructure.Serialization.Json.Newtonsoft/Serializer.cs
+++ src/MooVC.Infrastructure.Serialization.Json.Newtonsoft/Serializer.cs
@@ -1,66 +1,63 @@
-﻿namespace MooVC.Infrastructure.Serialization.Json.Newtonsoft
+﻿namespace MooVC.Infrastructure.Serialization.Json.Newtonsoft;
+
+using System;
+using System.IO;
+using System.Text;
+using global::Newtonsoft.Json;
+using MooVC.Compression;
+using MooVC.Serialization;
+using static System.String;
+using static MooVC.Infrastructure.Serialization.Json.Newtonsoft.Resources;
+
+public sealed class Serializer
+    : SynchronousSerializer
 {
-    using System;
-    using System.IO;
-    using System.Text;
-    using global::Newtonsoft.Json;
-    using MooVC.Compression;
-    using MooVC.Serialization;
-    using static System.String;
-    using static MooVC.Infrastructure.Serialization.Json.Newtonsoft.Resources;
+    public const int DefaultBufferSize = 1024;
+    public const int MinimumBufferSize = 8;
+    public static readonly Encoding DefaultEncoding = Encoding.UTF8;
+    private readonly Lazy<JsonSerializer> json;
 
-    public sealed class Serializer
-        : SynchronousSerializer
+    public Serializer(
+        int bufferSize = DefaultBufferSize,
+        Encoding? encoding = default,
+        ICompressor? compressor = default,
+        JsonSerializerSettings? settings = default)
+        : base(compressor: compressor)
     {
-        public const int DefaultBufferSize = 1024;
-        public const int MinimumBufferSize = 8;
-        public static readonly Encoding DefaultEncoding = Encoding.UTF8;
-        private readonly Lazy<JsonSerializer> json;
+        BufferSize = Math.Max(bufferSize, MinimumBufferSize);
+        Encoding = encoding ?? DefaultEncoding;
+        json = new Lazy<JsonSerializer>(() => JsonSerializer.CreateDefault(settings));
+    }
 
-        public Serializer(
-            int bufferSize = DefaultBufferSize,
-            Encoding? encoding = default,
-            ICompressor? compressor = default,
-            JsonSerializerSettings? settings = default)
-            : base(compressor: compressor)
+    public JsonSerializer Json => json.Value;
+
+    public int BufferSize { get; }
+
+    public Encoding Encoding { get; }
+
+    protected override T PerformDeserialize<T>(Stream source)
+    {
+        using var reader = new StreamReader(source);
+        using var text = new JsonTextReader(reader);
+
+        T? result = Json.Deserialize<T>(text);
+
+        if (result is null)
         {
-            BufferSize = Math.Max(bufferSize, MinimumBufferSize);
-            Encoding = encoding ?? DefaultEncoding;
-            json = new Lazy<JsonSerializer>(() => JsonSerializer.CreateDefault(settings));
+            throw new JsonSerializationException(Format(SerializerDeserializeFailure, typeof(T).AssemblyQualifiedName));
         }
 
-        public JsonSerializer Json => json.Value;
+        return result;
+    }
 
-        public int BufferSize { get; }
+    protected override void PerformSerialize<T>(T instance, Stream target)
+    {
+        using var writer = new StreamWriter(target, Encoding, BufferSize, true);
+        using var text = new JsonTextWriter(writer);
 
-        public Encoding Encoding { get; }
+        Json.Serialize(text, instance);
 
-        protected override T PerformDeserialize<T>(Stream source)
-        {
-            using var reader = new StreamReader(source);
-            using var text = new JsonTextReader(reader);
-
-            T? result = Json.Deserialize<T>(text);
-
-            if (result is null)
-            {
-                throw new JsonSerializationException(Format(
-                    SerializerDeserializeFailure,
-                    typeof(T).AssemblyQualifiedName));
-            }
-
-            return result;
-        }
-
-        protected override void PerformSerialize<T>(T instance, Stream target)
-        {
-            using var writer = new StreamWriter(target, Encoding, BufferSize, true);
-            using var text = new JsonTextWriter(writer);
-
-            Json.Serialize(text, instance);
-
-            text.Flush();
-            writer.Flush();
-        }
+        text.Flush();
+        writer.Flush();
     }
 }
